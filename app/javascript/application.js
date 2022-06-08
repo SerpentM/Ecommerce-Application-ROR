7db708c2904fe58ec613f357196112d9bce3507e
Prashant Mishra <prashant.mishra@mirraw.com>
2022-06-08 22:07:19 +0530

    Order Code Added

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,8 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+console.log("Hello World")
+var a = document.getElementById("order_pay_type")
+a.classList.add("btn");
+a.classList.add("select")
